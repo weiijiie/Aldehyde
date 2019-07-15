85febed39b1df7b7b333834592fd42afff699dd9
weiiijiiie <h_weijie@yahoo.com>
2019-07-15 16:30:36 +0800

    program now accepts output file name as argment

--- js/capitalize.js
+++ js/capitalize.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const fileName = process.argv[2];
-const outName = process.argv[3];
 
 fs.readFile(fileName, 'utf8', (err, data) => {
-   if (err) throw err;
+    if (err) throw err;
     writeToFileInCaps(data);
 });
 
 
 async function writeToFileInCaps(text) {
-    const 
     const [name, ext] = fileName.split('.');
-    fs.writeFile(`${name}-capitalized.${ext}`, text.toUpperCase(), 'utf8', err => {
+    const outName = process.argv[3] || `${name}-capitalized.${ext}`; 
+
+    fs.writeFile(outName, text.toUpperCase(), 'utf8', err => {
         if (err) throw err;
         console.log('Capitalized version written!');
     });
